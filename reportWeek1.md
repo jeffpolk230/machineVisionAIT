0402ad27dfb20a570d7e61113ea248671eea29b2
Jeff Chen <jeff.polk230@gmail.com>
2014-02-12 15:03:06 +0700

    little snap under pythonCV

--- reportWeek1.md
+++ reportWeek1.md
@@ -1,5 +1,8 @@
 ## Homework for week 1 and 2
 ---
 ### calibrate a camera of your choice using the OpenCV tools (or Matlab Camera Calibration Toolbox from Caltech)
+
 Using example calibration program from Opencv. 
-Following tutorial from: http://docs.opencv.org/doc/tutorials/calib3d/camera_calibration/camera_calibration.html
+Following tutorial from: [Opencv tutorial](http://docs.opencv.org/doc/tutorials/calib3d/camera_calibration/camera_calibration.html)
+
+### Extra: taking continuous photos using python Opencv
---
+++ snaps.py
@@ -0,0 +1,19 @@
+import cv2.cv as cv
+import time
+
+cv.NamedWindow("camera", cv.CV_WINDOW_NORMAL)
+# anyone knows where's CV_WINDOW_OPENGL? it's in the documentation
+# cv2 stuff!
+capture = cv.CaptureFromCAM(0)
+i=0
+while True:
+    img = cv.QueryFrame(capture)
+    cv.putText(img, "Press s for snap, Esc to quit")
+    cv.ShowImage("camera", img)
+    if cv.WaitKey(1) == ord('s'):
+    	print "snaped!"
+    	cv.SaveImage('pic{:>03}.jpg'.format(i), img)
+    	i +=1
+    if cv.WaitKey(1) == 27: # Esc
+        break
+#    time.sleep(1)
